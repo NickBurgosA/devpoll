7d5ac878ed2d98bb3fd79ec9d81101345ae2c777
NickBurgosA <nburgosa2@upao.edu.pe>
2017-06-18 22:36:04 -0500

    add foundation framework

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,21 @@ from .models import Question,Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Fechas', {'fields': ['pub_date']}),
+
+        ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+
+
 admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
